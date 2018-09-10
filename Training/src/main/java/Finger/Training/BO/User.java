dfebefab57761709e98a1b384e67ae1c5d5b4fbd
Dzmitry Parkhomenka <Dzmitry_parkhomenka@epam.com>
2018-09-10 19:21:36 +0300

    Add Training project

---
+++ Training/src/main/java/Finger/Training/BO/User.java
@@ -0,0 +1,14 @@
+package Finger.Training.BO;
+
+public class User {
+	private static final String login = "";
+	private static final String password = "";
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+}
---
+++ Training/src/main/java/Finger/Training/Page/LoginPage.java
@@ -0,0 +1,10 @@
+package Finger.Training.Page;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+	private static final By LOGIN_FIELD_LOCATOR = By.xpath("");
+	private static final By PASSWORD_FIELD_LOCATOR = By.xpath("");
+	
+	
+}
